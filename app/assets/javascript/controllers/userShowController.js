ae6e0bb08d0481cedd8e612bdbbd704a342f79bf
Youwei Lu <youwei_lu@hotmail.com>
2016-04-17 09:11:27 -0400

    Completed a function, for now.

---
+++ app/assets/javascript/controllers/userShowController.js
@@ -0,0 +1,3 @@
+angular.module('NoteWrangler').controller('NotesShowController',function(User,$scope,$routeParams){
+  $scope.user = User.get({id: $routeParams.id});
+});
--- app/assets/javascript/services/user.js
+++ app/assets/javascript/services/user.js
@@ -1,7 +1,4 @@
 angular.module("NoteWrangler").factory('Note', function($resource){
   return $resource('/notes/:id', {id: '@id'}, {
-    update: {
-      method: "PUT"
-    }
-  });
+
 })
